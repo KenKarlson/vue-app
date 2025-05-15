73cef979e185b6c7c4caa49c68832b21af68a7ef
Ken Karlson <javarecords@mail.ru>
2025-05-15 23:41:43 +0300

    Add new error & fix new error))))

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,14 @@
 <script setup>
+let header = "header top";
 let message = "Hello";
+let link = "<a href='/'>Ссыла</a>";
 </script>
 <template>
   <div>
+    <header class="header">{{ header }}</header>
     {{ message }}
+    <div v-html="link"></div>
+    <footer class="footer">footer</footer>
   </div>
 </template>
 
--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,35 @@
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
+import { fileURLToPath, URL } from "node:url";
 
-// https://vite.dev/config/
 export default defineConfig({
+  resolve: {
+    alias: {
+      "@": fileURLToPath(new URL("./src", import.meta.url)),
+      vue: "vue/dist/vue.esm-browser.js", // Явное указание сборки для браузера
+    },
+  },
+  server: {
+    headers: {
+      "Content-Security-Policy":
+        "default-src 'self' data:; " +
+        "script-src 'self' 'unsafe-inline' 'unsafe-eval' http://localhost:*; " +
+        "style-src 'self' 'unsafe-inline'; " +
+        "connect-src 'self' ws://localhost:*; " +
+        "img-src 'self' data: blob:",
+    },
+  },
   plugins: [vue()],
 });
+
+// import { defineConfig } from "vite";
+// import vue from "@vitejs/plugin-vue";
+
+// export default defineConfig({
+//   server: {
+//     headers: {
+//       "Content-Security-Policy": "script-src 'self' 'unsafe-inline'",
+//     },
+//   },
+//   plugins: [vue()],
+// });
