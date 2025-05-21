5736973741d73e66dc61e0702d410df9e446a71d
Ken Karlson <javarecords@mail.ru>
2025-05-21 08:30:04 +0300

    Add stat

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,15 @@
 <script setup>
+import Stat from "./components/Stat.vue";
 import Button from "./components/Button.vue";
 </script>
 <template>
   <main class="main">
+    <Stat />
     <Button>Сохранить</Button>
   </main>
 </template>
 
-<style scoped>
+<style lang="scss" scoped>
 .main {
   background-color: var(--color-bg-main);
   padding: 60px 50px;
---
+++ src/components/Stat.vue
@@ -0,0 +1,21 @@
+<script setup></script>
+<template>
+  <div class="stat">
+    <div class="stat__name">Влажность</div>
+    <div class="stat__value">90%</div>
+  </div>
+</template>
+<style lang="scss" scoped>
+.stat{
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  font-size: 16px;
+}
+.stat__name{  
+  font-weight: 700;
+}
+.stat__value{
+  font-weight: 500;
+}
+</style>
